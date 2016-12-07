8ba5479d8eefe6ffe72fade83107dc52b1e96d2e
Victor Rendina <vrendina@gmail.com>
2016-12-07 18:28:11 -0500

    Initial creation of data model class for holding movie data.

---
+++ app/src/main/java/software/level/udacity/popularmovies1/data/Movie.java
@@ -0,0 +1,13 @@
+package software.level.udacity.popularmovies1.data;
+
+/**
+ * Data model for Movie
+ */
+
+public class Movie {
+    public String mTitle;
+
+    public Movie(String title) {
+        mTitle = title;
+    }
+}
