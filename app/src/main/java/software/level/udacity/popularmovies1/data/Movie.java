dcb8b99e0c9a53814d5d6161edbafb003c64a3bb
Victor Rendina <vrendina@gmail.com>
2016-12-07 21:05:37 -0500

    Added fields from the API to the data model

--- app/src/main/java/software/level/udacity/popularmovies1/data/Movie.java
+++ app/src/main/java/software/level/udacity/popularmovies1/data/Movie.java
@@ -1,13 +1,20 @@
 package software.level.udacity.popularmovies1.data;
 
 /**
- * Data model for Movie
+ * Data model for Movie objects retrieved from The Movie Database
  */
-
 public class Movie {
-    public String mTitle;
 
-    public Movie(String title) {
-        mTitle = title;
-    }
+    public String backdrop_path;
+    public String homepage;
+    public int moviedb_id;
+    public double popularity;
+    public String poster_path;
+    public String release_date;
+    public int runtime;
+    public String title;
+    public String tagline;
+    public double vote_average;
+    public int vote_count;
+
 }
