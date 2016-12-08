9a5eaf9c8354fd50a4c441dba43016a03079420b
Victor Rendina <vrendina@gmail.com>
2016-12-07 20:22:25 -0500

    Added AsyncTask for downloading movie data

--- app/src/main/java/software/level/udacity/popularmovies1/MainActivity.java
+++ app/src/main/java/software/level/udacity/popularmovies1/MainActivity.java
@@ -1,6 +1,8 @@
 package software.level.udacity.popularmovies1;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -12,9 +14,12 @@ import android.widget.ProgressBar;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import software.level.udacity.popularmovies1.data.Movie;
+import software.level.udacity.popularmovies1.data.MovieParser;
 import software.level.udacity.popularmovies1.utilities.MovieRequestType;
 import software.level.udacity.popularmovies1.utilities.NetworkUtils;
 
@@ -30,6 +35,9 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
     // Holds a reference to the loading indicator
     private ProgressBar mProgressBar;
+
+    // Hold the current movie request type, by default we will get popular movies
+    private MovieRequestType mCurrentMovieRequestType = MovieRequestType.POPULAR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +61,7 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         // Picasso.with(context).load("http://i.imgur.com/DvpvklR.png").into(imageView);
 
+        fetchMovieData();
     }
 
 
@@ -73,6 +82,15 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         // Create the adapter and set it
         mMovieAdapter = new MovieAdapter(this);
         mRecyclerView.setAdapter(mMovieAdapter);
+    }
+
+    /**
+     * Kicks off the loading of movie data from The Movie Database
+     */
+    private void fetchMovieData() {
+        FetchMovieDataTask task = new FetchMovieDataTask(this);
+
+        task.execute(mCurrentMovieRequestType);
     }
 
 
@@ -126,5 +144,69 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         detailIntent.putExtra(Intent.EXTRA_TEXT, "id");
 
         startActivity(detailIntent);
+    }
+
+    /**
+     * AsyncTask that fetches movie data and parses it in the background before returning
+     * to the main thread to update the RecyclerView with the data.
+     */
+    public class FetchMovieDataTask extends AsyncTask<MovieRequestType, Void, ArrayList<Movie>> {
+
+        // The custom URL builder requires a context to be able to pull the api key from the strings
+        private Context mContext;
+
+        /**
+         * Create a new FetchMovieDataTask
+         * @param context Context from the creating activity
+         */
+        public FetchMovieDataTask(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /**
+         * Downloads data from the Movie Database and parses it in the background
+         * @param movieRequestTypes Either POPULAR or TOP_RATED movies can be fetched
+         * @return Parsed ArrayList containing Movie objects for the downloaded data
+         */
+        @Override
+        protected ArrayList<Movie> doInBackground(MovieRequestType... movieRequestTypes) {
+
+            // Generate the request URL
+            URL requestURL = null;
+            if(movieRequestTypes[0] == MovieRequestType.POPULAR) {
+                requestURL = NetworkUtils.buildURL(mContext, MovieRequestType.POPULAR);
+            }
+
+            if(movieRequestTypes[0] == MovieRequestType.TOP_RATED) {
+                requestURL = NetworkUtils.buildURL(mContext, MovieRequestType.TOP_RATED);
+            }
+
+            try {
+                // Execute the API call
+                String response = NetworkUtils.getResponseFromHttpUrl(requestURL);
+                Log.d(TAG, "Response: " + response);
+
+                // Parse the JSON data into an ArrayList of Movie objects
+                ArrayList<Movie> movies = MovieParser.parseMovieData(response);
+                Log.d(TAG, "Parsed response: " + movies.toString());
+
+                return movies;
+
+            } catch (IOException e) {
+                Log.e(TAG, e.toString());
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Movie> movies) {
+            super.onPostExecute(movies);
+        }
     }
 }
