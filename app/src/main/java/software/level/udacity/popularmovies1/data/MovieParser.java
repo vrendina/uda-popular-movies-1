621190ebb3d57a7c89da7390e9d2586cc75b7385
Victor Rendina <vrendina@gmail.com>
2016-12-09 22:16:52 -0500

    Added parsing for movie details

--- app/src/main/java/software/level/udacity/popularmovies1/data/MovieParser.java
+++ app/src/main/java/software/level/udacity/popularmovies1/data/MovieParser.java
@@ -24,6 +24,7 @@ public class MovieParser {
      * all of the movies. If a details for a single movie are request then the ArrayList will
      * only contain a single Movie object.
      * @param jsonData Data returned from the API call
+     * @param requestType MovieDB request type
      * @return ArrayList containing parsed Movie objects
      */
     public static ArrayList<Movie> parseMovieData(String jsonData, MovieRequestType requestType) throws JSONException {
@@ -40,23 +41,44 @@ public class MovieParser {
                 // Create a Movie object for each result and add it to the ArrayList
                 for(int i = 0; i < results.length(); i++) {
                     JSONObject result = results.getJSONObject(i);
-
-                    Movie movie = new Movie();
-                    movie.title = result.getString("title");
-                    movie.poster_path = result.getString("poster_path");
-                    movie.backdrop_path = result.getString("backdrop_path");
-                    movie.moviedb_id = result.getInt("id");
-                    movie.popularity = result.getDouble("popularity");
-                    movie.vote_average = result.getDouble("vote_average");
-                    movie.vote_count = result.getInt("vote_count");
-                    movie.overview = result.getString("overview");
-
+                    Movie movie = createMovieObject(result, requestType);
                     movieData.add(movie);
                 }
             }
         }
 
+        if(requestType == MovieRequestType.DETAILS) {
+            Movie movie = createMovieObject(jsonObject, requestType);
+            movieData.add(movie);
+        }
+
         return movieData;
+    }
+
+    /**
+     * Create a Movie object with the returned JSON data
+     * @param result JSONObject containing the movie data result
+     * @param requestType MovieDB request type
+     * @return Movie object with all fields extracted
+     */
+    private static Movie createMovieObject(JSONObject result, MovieRequestType requestType) throws JSONException {
+        Movie movie = new Movie();
+
+        movie.title = result.getString("title");
+        movie.poster_path = result.getString("poster_path");
+        movie.backdrop_path = result.getString("backdrop_path");
+        movie.moviedb_id = result.getInt("id");
+        movie.popularity = result.getDouble("popularity");
+        movie.vote_average = result.getDouble("vote_average");
+        movie.vote_count = result.getInt("vote_count");
+        movie.overview = result.getString("overview");
+
+        // If we have a detail request then they are additional fields we can grab
+        if(requestType == MovieRequestType.DETAILS) {
+            movie.runtime = result.getInt("runtime");
+        }
+
+        return movie;
     }
 
 }
